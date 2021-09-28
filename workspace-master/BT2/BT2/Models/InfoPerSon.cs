727850117ffca75e95b14c51026a4be686d11b4b
tungtran1998 <76432309+tungtran1998@users.noreply.github.com>
2021-09-28 14:07:16 +0700

    Update InfoPerSon.cs

--- workspace-master/BT2/BT2/Models/InfoPerSon.cs
+++ workspace-master/BT2/BT2/Models/InfoPerSon.cs
@@ -1,12 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace BT2.Models
 {
     public class InfoPerSon:PerSon
     {
+        [AllowHtml]
         public string Description { get; set; }
         public string Sex { get; set; }
     }
